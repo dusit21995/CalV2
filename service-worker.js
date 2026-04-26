4716a131b98806d47613ec1ee4e53365d1f7a34b
dusit21995 <150546783+dusit21995@users.noreply.github.com>
2026-04-26 22:42:24 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = "dusit-loadcalc-v7";
+
+const urlsToCache = [
+"./",
+"./index.html",
+"./style.css",
+"./script.js",
+"./manifest.json",
+"./logo.png"
+];
+
+self.addEventListener("install", event => {
+
+self.skipWaiting();
+
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urlsToCache))
+);
+
+});
+
+self.addEventListener("activate", event => {
+
+event.waitUntil(
+caches.keys().then(keys => {
+return Promise.all(
+keys.map(key => {
+if(key !== CACHE_NAME){
+return caches.delete(key);
+}
+})
+);
+})
+);
+
+self.clients.claim();
+
+});
+
+self.addEventListener("fetch", event => {
+
+event.respondWith(
+fetch(event.request)
+.then(response => {
+
+const clone = response.clone();
+
+caches.open(CACHE_NAME).then(cache => {
+cache.put(event.request, clone);
+});
+
+return response;
+
+})
+.catch(() => caches.match(event.request))
+);
+
+});
